16f2947d20936f37fa244bb3743af2688f9dae29
wubaoguo <wustrive_2008@126.com>
2018-04-26 15:47:41 +0800

    切换基础类库

--- src/main/java/com/wubaoguo/springboot/Application.java
+++ src/main/java/com/wubaoguo/springboot/Application.java
@@ -5,13 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages={"com.wubaoguo.springboot","org.wustrive.java.dao.jdbc.dao"})  
+@ComponentScan(basePackages = {"com.wubaoguo.springboot", "com.wustrive.lib.dao.jdbc.dao"})
 
-@SpringBootApplication 
+@SpringBootApplication
 @EnableCaching
 public class Application {
-	 
-	public static void main(String[] args){  
+
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }  
+    }
 }

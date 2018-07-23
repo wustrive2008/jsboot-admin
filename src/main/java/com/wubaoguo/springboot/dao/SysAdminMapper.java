432dccd5f3843151278ba9b4ee76b830703f125c
wubaoguo <wustrive_2008@126.com>
2018-07-23 16:21:14 +0800

    注释

--- src/main/java/com/wubaoguo/springboot/dao/SysAdminMapper.java
+++ src/main/java/com/wubaoguo/springboot/dao/SysAdminMapper.java
@@ -4,6 +4,14 @@ import com.wubaoguo.springboot.model.SysAdmin;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+/**
+ * Description: mybaits之Mapper-xml方式示例
+ *
+ * @author: wubaoguo
+ * @email: wustrive2008@gmail.com
+ * @date: 2018/7/23 15:41
+ * @Copyright: 2017-2018 dgztc Inc. All rights reserved.
+ */
 @Mapper
 public interface SysAdminMapper {
     int deleteByPrimaryKey(String id);
